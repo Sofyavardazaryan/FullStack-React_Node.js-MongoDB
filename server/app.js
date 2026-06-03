8f8773e183729d6f2149506f1a6c7f5523692f4c
Sofya <vardazaryansofia123@gmail.com>
2026-06-03 11:30:48 +0400

    update MongoDB Atlas

--- server/app.js
+++ server/app.js
@@ -9,53 +9,47 @@ const todoRouter = require("./routes/todo");
 
 const app = express();
 
-// DB
 mongoose
-  .connect("mongodb://127.0.0.1:27017/todo-app")
+  .connect(
+    "mongodb://vardazaryansofya_db_user:7S6iLdxCvlp0b2wB@ac-f9bfoqk-shard-00-00.s5i96nt.mongodb.net:27017,ac-f9bfoqk-shard-00-01.s5i96nt.mongodb.net:27017,ac-f9bfoqk-shard-00-02.s5i96nt.mongodb.net:27017/todo-app?ssl=true&replicaSet=atlas-qpga6z-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0",
+  )
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-// MIDDLEWARE
 app.use(express.json());
 
-// CORS for React (Vite)
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-  })
+  }),
 );
 
-// SESSION
 app.use(
   session({
     secret: "secret123",
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-      mongoUrl: "mongodb://127.0.0.1:27017/todo-app",
+      mongoUrl:
+        "mongodb://vardazaryansofya_db_user:7S6iLdxCvlp0b2wB@ac-f9bfoqk-shard-00-00.s5i96nt.mongodb.net:27017,ac-f9bfoqk-shard-00-01.s5i96nt.mongodb.net:27017,ac-f9bfoqk-shard-00-02.s5i96nt.mongodb.net:27017/todo-app?ssl=true&replicaSet=atlas-qpga6z-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0",
     }),
     cookie: {
       httpOnly: true,
       secure: false,
       maxAge: 1000 * 60 * 60 * 24,
     },
-  })
+  }),
 );
 
-// ROUTES
 app.use("/auth", authRouter);
 app.use("/todos", todoRouter);
 
-// TEST
 app.get("/", (req, res) => {
-  res.json({ message: "API running 🚀" });
+  res.json({ message: "API running" });
 });
 
-// ERROR HANDLER
 app.use((err, req, res, next) => {
-  console.error(err);
-
   res.status(err.status || 500).json({
     success: false,
     message: err.message || "Server Error",
